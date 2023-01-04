be1401a1d9d2f29666eed1a00817fc0946981fa4
Alejandro Fidececchi <ale.fidececchi@gmail.com>
2023-01-04 17:39:37 -0300

    pagination for main container added

--- src/controllers/movie/get.js
+++ src/controllers/movie/get.js
@@ -3,19 +3,26 @@ const Movie = require('../../models/Movie.js');
 
 const getMovies = async (req = request, res = response) => {
 
-    let { categories, title } = req.query
+    let { categories, limit, page, title } = req.query
     let movies;
+    let count;
 
     //AGREGAR PAGINADO PARA PAGINA PRINCIPAL
     try {
+        console.log('hello from backend')
         if (categories) {
-            movies = await getMoviesCategories(categories)
+            let obj = await getMoviesCategories(categories, page, limit)
+            count = obj.count
+            movies = obj.movies
         } else if (title) {
-            movies = await getMovieTitle(title)
+            let obj = await getMovieTitle(title, page, limit)
+            count = obj.count
+            movies = obj.movies
         } else {
-            movies = await Movie.find()
+            count = await Movie.find().count()
+            movies = await Movie.find().sort({ _id: -1 }).skip((page - 1) * limit).limit(limit)
         }
-        return res.status(200).json({ movies })
+        return res.status(200).json({ movies, page, count })
     } catch (error) {
         return res.status(404).json({ message: 'Hubo algún error', error })
     }
@@ -31,12 +38,14 @@ const getMovieId = async (req = request, res = response) => {
     }
 }
 
-const getMoviesCategories = async (categories) => {
+const getMoviesCategories = async (categories, page, limit) => {
     try {
         const arr = categories.split(',')
         const query = { $all: arr }
-        const movies = await Movie.find({ category: query })
-        return movies
+        // console.log('PAGE: ', page, 'LIMIT: ', limit)
+        const count = await Movie.find({ category: query }).count()
+        const movies = await Movie.find({ category: query }).sort({ _id: -1 }).skip((page - 1) * limit).limit(limit)
+        return ({ movies, count })
     } catch (error) {
         return res.status(404).json({ message: 'Algo salío mal con las categorías seleccionadas', error })
     }
@@ -44,18 +53,25 @@ const getMoviesCategories = async (categories) => {
 
 const getMoviesDashboard = async (req = request, res = response) => {
 
+    const { title } = req.query
+    let movies;
     try {
-        const movies = await Movie.find({}, { title: 1, link_img: 1 })
+        if (title) {
+            movies = await Movie.find({ title: new RegExp(title, 'i') }, { title: 1, link_img: 1 })
+        } else {
+            movies = await Movie.find({}, { title: 1, link_img: 1 })
+        }
         return res.status(200).json({ movies })
     } catch (error) {
         return res.status(404).json({ message: 'Hubo algún error', error })
     }
 }
 
-const getMovieTitle = async (title) => {
+const getMovieTitle = async (title, page, limit) => {
     try {
-        const movies = await Movie.find({ title: new RegExp(title, 'i') })
-        return movies;
+        const count = await Movie.find({ title: new RegExp(title, 'i') }).count()
+        const movies = await Movie.find({ title: new RegExp(title, 'i') }).sort({ _id: -1 }).skip((page - 1) * limit).limit(limit)
+        return ({ count, movies });
     } catch (error) {
         return new Error('Algo salió mal con el titulo agregado')
     }
--- src/controllers/serie/get.js
+++ src/controllers/serie/get.js
@@ -3,18 +3,19 @@ const Serie = require('../../models/Serie.js');
 
 const getSeries = async (req = request, res = response) => {
 
-    let { categories, title } = req.query
-    let series;
+    let { categories, limit, page, title } = req.query
+    let obj = {};
 
     try {
         if (categories) {
-            series = await getSeriesCategory(categories)
+            obj = await getSeriesCategory(categories, page, limit)
         } else if (title) {
-            series = await getSerieTitle(title)
+            obj = await getSerieTitle(title, page, limit)
         } else {
-            series = await Serie.find()
+            obj.count = await Serie.find().count()
+            obj.series = await Serie.find().sort({ _id: -1 }).skip((page - 1) * limit).limit(limit)
         }
-        return res.status(200).json({ series })
+        return res.status(200).json({ count: obj.count, page, series: obj.series })
     } catch (error) {
         return res.status(404).json({ message: 'Algo salió mal', error })
     }
@@ -31,30 +32,38 @@ const getSerieId = async (req = request, res = response) => {
     }
 }
 
-const getSeriesCategory = async (categories) => {
+const getSeriesCategory = async (categories, page, limit) => {
     try {
         const arr = categories.split(',')
-        const query = { $all: arr }
-        const series = await Serie.find({ category: query })
-        return series
+        const count = await Serie.find({ category: { $all: arr } }).count()
+        const series = await Serie.find({ category: { $all: arr } }).sort({ _id: -1 }).skip((page - 1) * limit).limit(limit)
+        return ({ count, series })
     } catch (error) {
         return res.status(404).json({ message: 'Algo salío mal con las categorías seleccionadas', error })
     }
 }
 
 const getSeriesDashboard = async (req = request, res = response) => {
+
+    const { title } = req.query
+    let series;
     try {
-        const series = await Serie.find({}, { title: 1, link_img: 1 })
+        if (title) {
+            series = await Serie.find({ title: new RegExp(title, 'i') }, { title: 1, link_img: 1 })
+        } else {
+            series = await Serie.find({}, { title: 1, link_img: 1 })
+        }
         return res.status(200).json({ series })
     } catch (error) {
         return res.status(404).json({ message: 'Hubo algún error', error })
     }
 }
 
-const getSerieTitle = async (title) => {
+const getSerieTitle = async (title, page, limit) => {
     try {
-        const series = await Serie.find({ title: new RegExp(title, 'i') })
-        return series
+        const count = await Serie.find({ title: new RegExp(title, 'i') }).count()
+        const series = await Serie.find({ title: new RegExp(title, 'i') }).sort({ _id: -1 }).skip((page - 1) * limit).limit(limit)
+        return ({ count, series })
     } catch (error) {
         return new Error('Algo salió mal con el titulo')
     }
